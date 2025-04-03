006df589ed5a574a5f378465bdaa096239c4660c
raid313 <rshari24@student.aau.dk>
2025-04-03 13:11:29 +0200

    projekt

---
+++ src/components/forsiden.vue
@@ -0,0 +1,46 @@
+<template>
+ <div>
+<div class = "login-signup">
+ <button> login/opret </button>   
+</div>
+
+<div class = "searchnew">
+ <button> søg ny opskrift </button>
+ </div>
+
+<div class = "oldused">
+ <button> historik </button>
+ </div>
+
+<div class = "favorite">
+<button> favorit </button>
+</div>
+ </div>
+</template>
+
+<style>
+.login-signup{
+position: absolute;
+top: 20px; 
+right: 20px;
+}
+
+.searchnew{
+position: absolute;
+top: 40%;
+right: 48%; 
+}
+
+.oldused{
+position: absolute;
+top: 50%;
+right: 50%;
+}
+
+.favorite{
+position: absolute;
+top: 60%;
+right: 50%;    
+}
+
+</style>
